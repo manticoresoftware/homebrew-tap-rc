722430a83788d50315827d1f198fb9debbaf4942
Sergey Nikolaev <prostuda@academ.org>
2021-12-23 17:38:38 +0700

    Release 4.2.0 hf
    Closes #7.
    
    Signed-off-by: Sergey Nikolaev <28719845+sanikolaev@users.noreply.github.com>

--- Formula/manticoresearch.rb
+++ Formula/manticoresearch.rb
@@ -1,14 +1,17 @@
 class Manticoresearch < Formula
   desc "Open source database for search"
   homepage "https://www.manticoresearch.com"
-  url "https://github.com/manticoresoftware/manticoresearch/archive/refs/tags/4.2.0.tar.gz"
-  sha256 "6b4af70fcc56b40aa83e606240b237e47e54c0bfbfdd32c47788d59469ef7146"
+  url "https://repo.manticoresearch.com/repository/manticoresearch_source/release/manticore-4.2.0-211223-15e927b28-release-source.tar.gz"
+  sha256 "da2c3af17e62960244a536c3767a2978d36bec3d403293e4f51a4a6718e9c2cb"
   license "GPL-2.0-only"
   version_scheme 1
 
   bottle do
     root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticoresearch-4.2.0"
-    sha256 big_sur: "566e7f33753c1a293bb1b2db2756ad84bf5c858fa5733c976623ed2eb0faa609"
+    sha256 arm64_big_sur: "a9ae578b05c3da46cedc07dd428d94a856aeae7f3ef80a0f405bf89b8cde893a"
+    sha256 big_sur:       "5dc376aa20241233b76e2ec2c1d4e862443a0250916b2838a1ff871e8a6dc2c5"
+    sha256 catalina:      "924afbbc16549d8c2b80544fd03104ff8c17a4b1460238e3ed17a1313391a2af"
+    sha256 mojave:        "678d338adc7d6e8c352800fe03fc56660c796bd6da23eda2b1411fed18bd0d8d"
   end
 
   depends_on "boost" => :build
