2fcb5758a9195389dc01110a9a504b65a1f56ae0
Dmitrii Kuzmenkov <dmitry@wagh.ru>
2022-09-19 16:42:10 +0700

    Update manticoresearch formula due to audit errors

--- Formula/manticoresearch.rb
+++ Formula/manticoresearch.rb
@@ -8,16 +8,16 @@ class Manticoresearch < Formula
 
   bottle do
     root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticoresearch-5.0.2"
-    sha256 big_sur: "d611017196be4acfe7bb0e0f85f011d033a2a2e76b8e76e70ae9ca885d4cb808"
-    sha256 catalina: "3c57d87bdf999d0e6e647b398e221d5511f2c729b8b66face9475d3b075c830d"
     sha256 arm64_big_sur: "2ef4e45e17ed6d92eb033b59512ccf355703d1126c7786fda7bf1a70c1439e18"
+    sha256 big_sur:       "d611017196be4acfe7bb0e0f85f011d033a2a2e76b8e76e70ae9ca885d4cb808"
+    sha256 catalina:      "3c57d87bdf999d0e6e647b398e221d5511f2c729b8b66face9475d3b075c830d"
   end
 
   depends_on "boost" => :build
   depends_on "cmake" => :build
   depends_on "libpq" => :build
   depends_on "mysql" => :build
-  depends_on "postgresql" => :build
+  depends_on "postgresql@14" => :build
   depends_on "openssl@1.1"
 
   conflicts_with "sphinx", because: "manticore is a fork of sphinx"
