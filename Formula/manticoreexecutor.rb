f46287e515cf96dd0c6f03cd7b49b86b7f30bf83
Dmitrii Kuzmenkov <1183502+dmitrykuzmenkov@users.noreply.github.com>
2022-09-20 05:23:50 +0000

    manticoreexecutor: add 0.1.0 bottle.

--- Formula/manticoreexecutor.rb
+++ Formula/manticoreexecutor.rb
@@ -4,6 +4,11 @@ class Manticoreexecutor < Formula
   url "https://github.com/manticoresoftware/executor/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "ba47ed71767e942001861112bfb04b9470a728ec607c40dcf9866597cdb17633"
 
+  bottle do
+    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticoreexecutor-0.1.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "641c246014192ccb38c01387871b091df2cd377598f9806ff5849b33ad3f1805"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "bison" => :build
