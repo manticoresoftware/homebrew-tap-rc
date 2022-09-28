e361fb10ca4aeb7f9466bf1107dd24c0f26e0c78
Sergey Nikolaev <prostuda@academ.org>
2022-09-28 14:08:55 +0700

    Update manticoresearch.rb

--- Formula/manticoresearch.rb
+++ Formula/manticoresearch.rb
@@ -10,8 +10,8 @@ class Manticoresearch < Formula
     root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticoresearch-5.0.2"
     rebuild 1
     sha256 arm64_big_sur: "2ef4e45e17ed6d92eb033b59512ccf355703d1126c7786fda7bf1a70c1439e18"
-    sha256 big_sur: "552c6c732e1c88d97b3c296766e7e4eafdcee2a982da42a4c76ea9cb503c82b8"
-    sha256 catalina: "3c57d87bdf999d0e6e647b398e221d5511f2c729b8b66face9475d3b075c830d"
+    sha256 big_sur:       "552c6c732e1c88d97b3c296766e7e4eafdcee2a982da42a4c76ea9cb503c82b8"
+    sha256 catalina:      "3c57d87bdf999d0e6e647b398e221d5511f2c729b8b66face9475d3b075c830d"
   end
 
   depends_on "boost" => :build
