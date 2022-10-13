8362e7c4f9ab448f95c6ee1241bf534fd0fb901c
Don Hardman <59067742+donhardman@users.noreply.github.com>
2022-10-13 11:14:26 +0000

    manticore-backup: update 0.2.13 bottle.

--- Formula/manticore-backup.rb
+++ Formula/manticore-backup.rb
@@ -5,7 +5,9 @@ class ManticoreBackup < Formula
   sha256 "afc97a8261ec75b3fb6c7d22f032fdc227faf137891a7575622c496e153f9295"
 
   bottle do
-    rebuild 1
+    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-backup-0.2.13"
+    sha256 cellar: :any_skip_relocation, monterey: "e0fd0326a4e508f0eef9e6e76934ffedff0531ee80b40c78cb81053d76720f5c"
+    sha256 cellar: :any_skip_relocation, big_sur:  "047abfe0246c884a9f3710714c7f78ebc2848b58f94dc443d617efb22708e018"
   end
 
   depends_on "php" => :build
