f8bbfc7692609e899bad4a9b839819d07e596a82
Don Hardman <don.hardman@muvon.io>
2022-10-14 22:49:07 +0700

    Update manticore-backup version to 0.2.19
    Closes #35.
    
    Signed-off-by: Don Hardman <59067742+donhardman@users.noreply.github.com>

--- Formula/manticore-backup.rb
+++ Formula/manticore-backup.rb
@@ -1,11 +1,11 @@
 class ManticoreBackup < Formula
   desc "Official tool for backups of Manticore Search"
   homepage "https://github.com/manticoresoftware/manticoresearch-backup"
-  url "https://github.com/manticoresoftware/manticoresearch-backup/archive/refs/tags/v0.2.17.tar.gz"
-  sha256 "563ee88d6fe5fb5e38eb566fbd05399b3e280130ce2bec399078825057c1e3ce"
+  url "https://github.com/manticoresoftware/manticoresearch-backup/archive/refs/tags/v0.2.19.tar.gz"
+  sha256 "cf8686374018437381e352425db4a78c8ecfc467fbc4144dda743e260b54ba6e"
 
   bottle do
-    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-backup-0.2.17"
+    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-backup-0.2.19"
     sha256 cellar: :any_skip_relocation, monterey: "d98ccdeca65077a6824d7abc27adf655dea7f4e1f063efa171cdf7e94ae90b42"
     sha256 cellar: :any_skip_relocation, big_sur:  "ad03f485ab31811e3891b75839f97aff23b0727e41240fe7ec0269582f37d15a"
   end
