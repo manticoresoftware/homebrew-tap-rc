08b444ebedc1d0f69cc2e7c25975d0d44a52e04f
Don Hardman <don.hardman@muvon.io>
2023-01-05 10:51:32 +0000

    Make it possible to update same semver with different builds
    Closes #42.
    
    Signed-off-by: Don Hardman <59067742+donhardman@users.noreply.github.com>

--- Formula/manticoresearch.rb
+++ Formula/manticoresearch.rb
@@ -1,18 +1,19 @@
 class Manticoresearch < Formula
   desc "Open source database for search"
   homepage "https://www.manticoresearch.com"
-  url "https://github.com/manticoresoftware/manticoresearch/archive/refs/tags/5.0.3.tar.gz"
-  sha256 "416b9e609529af9cf9570b41e3e770de9511c5ed4d0c27530bfec7da396b13b2"
+  url "https://github.com/manticoresoftware/manticoresearch.git", revision: "cc13519f4590466b92cb5ce1cc99fd1d03fa2a9c"
+  version "5.0.3-2022010512-cc13519"
   license "GPL-2.0"
   version_scheme 1
+  head "https://github.com/manticoresoftware/manticoresearch.git"
 
   bottle do
     root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticoresearch-5.0.3"
     rebuild 1
-    sha256 monterey: "03cb693d15a62d78d4913fb35e2bd6c987fe42875c3e2b1be4a54bee7bc4e8de"
-    sha256 big_sur:  "169a865e9664f19e6078e0569542703a193c0837a9340e83ab860627f81179ef"
-    sha256 catalina: "a9fb628b2bee4c2390368d4a2593b200f0c509e74b41b441fe349e692a7945a2"
     sha256 arm64_monterey: "a721a28aa4e47223b9c78bc9cdabcfe4171226853533c6a0941c4c55bd54e39f"
+    sha256 monterey:       "03cb693d15a62d78d4913fb35e2bd6c987fe42875c3e2b1be4a54bee7bc4e8de"
+    sha256 big_sur:        "169a865e9664f19e6078e0569542703a193c0837a9340e83ab860627f81179ef"
+    sha256 catalina:       "a9fb628b2bee4c2390368d4a2593b200f0c509e74b41b441fe349e692a7945a2"
   end
 
   depends_on "boost" => :build
