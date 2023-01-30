331cd626480020eed2a5b8a6232b57df0c5ff43b
Sergey Nikolaev <prostuda@academ.org>
2023-01-30 18:54:41 +0700

    Update manticore-columnar-lib.rb
    Closes #44.
    
    Signed-off-by: Sergey Nikolaev <28719845+sanikolaev@users.noreply.github.com>

--- Formula/manticore-columnar-lib.rb
+++ Formula/manticore-columnar-lib.rb
@@ -1,13 +1,12 @@
 class ManticoreColumnarLib < Formula
-  desc "Column-oriented storage library"
+  desc "Manticore Columnar Library"
   homepage "https://github.com/manticoresoftware/columnar/"
   url "https://github.com/manticoresoftware/columnar/archive/1.16.1.tar.gz"
-  sha256 "2778e4d8c2fa065cc56d6071f602e5a8c76a0223456f6e046353d62639983d8b"
+  url "https://github.com/manticoresoftware/columnar.git", revision: "d0da46228b3c06b7a30aad9c9b8abce1ee3cba71"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-columnar-lib-1.16.1"
-    rebuild 1
     sha256 cellar: :any_skip_relocation, monterey: "755471f795b0eadc759d6d83ab502e426edebe12a20e5bcc8fc29ddbe0a58540"
     sha256 cellar: :any_skip_relocation, big_sur:  "8dca2c99a7e2acc5696913c81b898e6b2bee3d584cc3afd14ac1cf046780da4e"
   end
