8c6dbc26ead9382354f727da8e9679ff57200588
Don Hardman <don.hardman@muvon.io>
2023-01-31 12:08:37 +0000

    Try to use branch also for buddy release
    Closes #53.
    
    Signed-off-by: Don Hardman <59067742+donhardman@users.noreply.github.com>

--- Formula/manticore-buddy.rb
+++ Formula/manticore-buddy.rb
@@ -3,18 +3,11 @@ require "fileutils"
 class ManticoreBuddy < Formula
   desc "Manticore Search's sidecar which helps it with various tasks"
   homepage "https://github.com/manticoresoftware/manticoresearch-buddy"
-  url "https://github.com/manticoresoftware/manticoresearch-buddy.git", revision: "0.3.4"
+  url "https://github.com/manticoresoftware/manticoresearch-buddy.git", branch: "0.3.4", revision: "8cb84c11a328999226ecdd2dbb67ab8633176fa2"
   version "0.3.4-2023013117-8cb84c1"
   license "GPL-2.0"
   version_scheme 1
   head "https://github.com/manticoresoftware/manticoresearch-buddy.git"
-
-  bottle do
-    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-buddy-0.3.4-2023013117-8cb84c1"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, monterey: "85d67c76f7fb92eae46887909e596342e1be4484f0bf21c8acca079bea09f640"
-    sha256 cellar: :any_skip_relocation, big_sur:  "82546489256a552ec353c8f4dc6dfd386670cc6648aab5a6d495ff2d9d559632"
-  end
 
   depends_on "composer" => :build
   depends_on "php" => :build
