a23b39d5e991d8c6789bbe12561bff59e9f316f3
Don Hardman <59067742+donhardman@users.noreply.github.com>
2023-01-31 12:33:16 +0000

    manticore-buddy: add 0.3.4-2023013117-8cb84c1 bottle.

--- Formula/manticore-buddy.rb
+++ Formula/manticore-buddy.rb
@@ -9,6 +9,13 @@ class ManticoreBuddy < Formula
   version_scheme 1
   head "https://github.com/manticoresoftware/manticoresearch-buddy.git"
 
+  bottle do
+    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-buddy-0.3.4-2023013117-8cb84c1"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, monterey: "ef7810c5f575791be6a149f6ff20e725412b3693bc31b90bd67a88466d79ee1d"
+    sha256 cellar: :any_skip_relocation, big_sur:  "8ce44ec840a078e5968a1a4912f1120189a3e71e944123c9b91b438c75ca0eb7"
+  end
+
   depends_on "composer" => :build
   depends_on "php" => :build
   depends_on "php" => :test
