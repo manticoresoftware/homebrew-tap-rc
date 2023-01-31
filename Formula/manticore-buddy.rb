1ef98e152cc492e8261b880643efcb3543e5f9cb
Don Hardman <don.hardman@muvon.io>
2023-01-31 10:51:35 +0000

    Use branch for buddy because commits differ from branch and main
    Closes #52.
    
    Signed-off-by: Don Hardman <59067742+donhardman@users.noreply.github.com>

--- Formula/manticore-buddy.rb
+++ Formula/manticore-buddy.rb
@@ -3,17 +3,11 @@ require "fileutils"
 class ManticoreBuddy < Formula
   desc "Manticore Search's sidecar which helps it with various tasks"
   homepage "https://github.com/manticoresoftware/manticoresearch-buddy"
-  url "https://github.com/manticoresoftware/manticoresearch-buddy.git", revision: "8cb84c11a328999226ecdd2dbb67ab8633176fa2"
+  url "https://github.com/manticoresoftware/manticoresearch-buddy.git", revision: "0.3.4"
   version "0.3.4-2023013117-8cb84c1"
   license "GPL-2.0"
   version_scheme 1
   head "https://github.com/manticoresoftware/manticoresearch-buddy.git"
-
-  bottle do
-    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-buddy-0.3.4-2023013117-8cb84c1"
-    sha256 cellar: :any_skip_relocation, monterey: "22f2bc400273b804d61c18a4046f9aec93a0ef3ccd7fd04975f936396978ef28"
-    sha256 cellar: :any_skip_relocation, big_sur:  "25b6e93612f884edbdced9a84d0b79eefaa24cea2398ca6e37b6dba154e6f10f"
-  end
 
   depends_on "composer" => :build
   depends_on "php" => :build
