98f0497dac08c60209bb6a7077a6a8a65bc67ca3
Don Hardman <59067742+donhardman@users.noreply.github.com>
2023-02-02 07:10:05 +0000

    manticore-backup: add 0.5.2-2023020213-2745fad bottle.

--- Formula/manticore-backup.rb
+++ Formula/manticore-backup.rb
@@ -7,6 +7,12 @@ class ManticoreBackup < Formula
   version_scheme 1
   head "https://github.com/manticoresoftware/manticoresearch-backup.git"
 
+  bottle do
+    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-backup-0.5.2-2023020213-2745fad"
+    sha256 cellar: :any_skip_relocation, monterey: "47d31ca22f001c9640975e995ab79c818c65f956e8231833688ceeb6658f596e"
+    sha256 cellar: :any_skip_relocation, big_sur:  "b1597a900a9b96e65d9f6da3ee214957e2f9c0ff8b5311a6e781365b77824dd5"
+  end
+
   depends_on "composer" => :build
   depends_on "php" => :build
   depends_on "php" => :test
