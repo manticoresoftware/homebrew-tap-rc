cc1eaf9e5e094f06443191a7df7129f93c65924f
Don Hardman <don.hardman@muvon.io>
2023-02-02 13:32:38 +0000

    Update version of backup with fixed build for macos

--- Formula/manticore-backup.rb
+++ Formula/manticore-backup.rb
@@ -1,17 +1,11 @@
 class ManticoreBackup < Formula
   desc "Official tool for backups of Manticore Search"
   homepage "https://github.com/manticoresoftware/manticoresearch-backup"
-  url "https://github.com/manticoresoftware/manticoresearch-backup.git", revision: "2745fad0f88bac936bcb261b02074b2f925c59d3"
-  version "0.5.2-2023020213-2745fad"
+  url "https://github.com/manticoresoftware/manticoresearch-backup.git", revision: "4a3793286c03860ce1b0c46ebee6c6f58a1948de"
+  version "0.5.2-2023020221-4a37932"
   license "GPL-2.0"
   version_scheme 1
   head "https://github.com/manticoresoftware/manticoresearch-backup.git"
-
-  bottle do
-    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-backup-0.5.2-2023020213-2745fad"
-    sha256 cellar: :any_skip_relocation, monterey: "47d31ca22f001c9640975e995ab79c818c65f956e8231833688ceeb6658f596e"
-    sha256 cellar: :any_skip_relocation, big_sur:  "b1597a900a9b96e65d9f6da3ee214957e2f9c0ff8b5311a6e781365b77824dd5"
-  end
 
   depends_on "composer" => :build
   depends_on "php" => :build
