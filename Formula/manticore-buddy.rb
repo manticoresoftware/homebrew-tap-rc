f5bb1822f86cd7cd965c7f59e6f5577720985945
Don Hardman <don.hardman@muvon.io>
2023-02-02 09:10:00 +0000

    Update buddy version
    Closes #56.
    
    Signed-off-by: Don Hardman <59067742+donhardman@users.noreply.github.com>

--- Formula/manticore-buddy.rb
+++ Formula/manticore-buddy.rb
@@ -3,17 +3,11 @@ require "fileutils"
 class ManticoreBuddy < Formula
   desc "Manticore Search's sidecar which helps it with various tasks"
   homepage "https://github.com/manticoresoftware/manticoresearch-buddy"
-  url "https://github.com/manticoresoftware/manticoresearch-buddy.git", branch: "0.3.4", revision: "ff8a045d2140279b5e7ba0db0da02e8c013bb8df"
-  version "0.3.4-2023020213-ff8a045"
+  url "https://github.com/manticoresoftware/manticoresearch-buddy.git", branch: "0.3.4", revision: "804832e779a5adfe180137c59bde207fc4f77f80"
+  version "0.3.4-2023020216-804832e"
   license "GPL-2.0"
   version_scheme 1
   head "https://github.com/manticoresoftware/manticoresearch-buddy.git"
-
-  bottle do
-    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-buddy-0.3.4-2023020213-ff8a045"
-    sha256 cellar: :any_skip_relocation, monterey: "1f2ded39e334eeb9ff91d49fbbe9ed633408cbfb31722fc901ead2bd07caaecb"
-    sha256 cellar: :any_skip_relocation, big_sur:  "ca400cea838ee85edf22c263a7201d1b5ac5ea9c1e28ca662f2b0cd3f36f5514"
-  end
 
   depends_on "composer" => :build
   depends_on "php" => :build
