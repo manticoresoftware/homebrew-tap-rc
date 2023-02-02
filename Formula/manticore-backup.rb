d5ece37bda8a7d7a487580d9382f990ec72e69b8
Don Hardman <don.hardman@muvon.io>
2023-02-02 06:16:00 +0000

    Update versions of backup and buddy packages
    Closes #55.
    
    Signed-off-by: Don Hardman <59067742+donhardman@users.noreply.github.com>

--- Formula/manticore-backup.rb
+++ Formula/manticore-backup.rb
@@ -1,17 +1,11 @@
 class ManticoreBackup < Formula
   desc "Official tool for backups of Manticore Search"
   homepage "https://github.com/manticoresoftware/manticoresearch-backup"
-  url "https://github.com/manticoresoftware/manticoresearch-backup.git", revision: "324cfbcb42e86b51244e69a850e12a46edd37c7c"
-  version "0.5.2-2023012611-324cfbc"
+  url "https://github.com/manticoresoftware/manticoresearch-backup.git", revision: "2745fad0f88bac936bcb261b02074b2f925c59d3"
+  version "0.5.2-2023020213-2745fad"
   license "GPL-2.0"
   version_scheme 1
   head "https://github.com/manticoresoftware/manticoresearch-backup.git"
-
-  bottle do
-    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-backup-0.5.2-2023012611-324cfbc"
-    sha256 cellar: :any_skip_relocation, monterey: "27a6153c220fa0c9b276040b6314feab1165b84f44cfe1d18ccf53ecfd4f5d01"
-    sha256 cellar: :any_skip_relocation, big_sur:  "3d49adda419daeecf616a1251feab6da6db76eb842377776dc230c48c7f67dbd"
-  end
 
   depends_on "composer" => :build
   depends_on "php" => :build
--- Formula/manticore-buddy.rb
+++ Formula/manticore-buddy.rb
@@ -3,17 +3,11 @@ require "fileutils"
 class ManticoreBuddy < Formula
   desc "Manticore Search's sidecar which helps it with various tasks"
   homepage "https://github.com/manticoresoftware/manticoresearch-buddy"
-  url "https://github.com/manticoresoftware/manticoresearch-buddy.git", branch: "0.3.4", revision: "2e1c62374aca30b703c73dc75bc63c737b94f779"
-  version "0.3.4-2023013121-2e1c623"
+  url "https://github.com/manticoresoftware/manticoresearch-buddy.git", branch: "0.3.4", revision: "ff8a045d2140279b5e7ba0db0da02e8c013bb8df"
+  version "0.3.4-2023020213-ff8a045"
   license "GPL-2.0"
   version_scheme 1
   head "https://github.com/manticoresoftware/manticoresearch-buddy.git"
-
-  bottle do
-    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-buddy-0.3.4-2023013121-2e1c623"
-    sha256 cellar: :any_skip_relocation, monterey: "909b5219b8e06d892543a1643ca51e948fe30a811532ac1756aa10fe0509e72b"
-    sha256 cellar: :any_skip_relocation, big_sur:  "b281b73db5fa7f1a4f382124a3b747f085948fead161528a7f54d7604de3de43"
-  end
 
   depends_on "composer" => :build
   depends_on "php" => :build
