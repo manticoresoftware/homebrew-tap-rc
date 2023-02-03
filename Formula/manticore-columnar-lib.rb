5d08716290f8bf92ad32f4fcb3f8b87bd15cbb5b
Sergey Nikolaev <28719845+sanikolaev@users.noreply.github.com>
2023-02-03 06:27:27 +0000

    manticore-columnar-lib: update 2.0.0-2023013023-a7c703d bottle.

--- Formula/manticore-columnar-lib.rb
+++ Formula/manticore-columnar-lib.rb
@@ -8,9 +8,9 @@ class ManticoreColumnarLib < Formula
   head "https://github.com/manticoresoftware/columnar.git"
 
   bottle do
-    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-columnar-lib-1.16.1-2023012002-d0da462"
-    sha256 cellar: :any_skip_relocation, monterey: "b0fc85d8e763c8c7ab993032a7e2aaff9cb1a037700ca86c62977e69fcd03f5a"
-    sha256 cellar: :any_skip_relocation, big_sur:  "9262e53bdc065f752484b1060bb94f010f73d23aa39c7a1fb589e613a71736b2"
+    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-columnar-lib-2.0.0-2023013023-a7c703d"
+    sha256 cellar: :any_skip_relocation, monterey: "7944dccf08985c6da253f781a82d08be3af1fa0de4aa39fad33e37b12375d2d8"
+    sha256 cellar: :any_skip_relocation, big_sur:  "2b58822eec2c957fff7b89d9aea67f6b16512801899b796e39db1a45befd30b9"
   end
 
   depends_on "boost" => :build
