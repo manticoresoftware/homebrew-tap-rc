bd493d6fbf423b6290a6c96c8b81709a85a0d89b
Sergey Nikolaev <prostuda@academ.org>
2023-02-03 14:21:20 +0700

    bottle for manticoresearch m1 ventura

--- Formula/manticoresearch.rb
+++ Formula/manticoresearch.rb
@@ -11,6 +11,7 @@ class Manticoresearch < Formula
     root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticoresearch-5.0.3-2023020218-7542cb7"
     sha256 monterey: "142f717f724c45ee23367e47b835fb3f7e1e99a6cee7ec85afafcf3df39ca616"
     sha256 big_sur:  "3d51359b31f0d8f18a93b4446b0c9ef46afc8ea625a748ef8a534927cded71b4"
+    sha256 arm64_ventura: "25c05bd56597f30a6d75f1e21b9facb5504c640903b35eca26459f0e44bbb53e"
   end
 
   depends_on "boost" => :build
