8db9caad47444240d2213f5022f4c3e023498ac3
Don Hardman <59067742+donhardman@users.noreply.github.com>
2023-02-05 10:30:38 +0000

    manticore-executor: add 0.6.2-2023012313-d95e43e bottle.

--- Formula/manticore-executor.rb
+++ Formula/manticore-executor.rb
@@ -7,6 +7,13 @@ class ManticoreExecutor < Formula
   version_scheme 1
   head "https://github.com/manticoresoftware/executor.git"
 
+  bottle do
+    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-executor-0.6.2-2023012313-d95e43e"
+    rebuild 1
+    sha256 monterey: "c36fda714fce10a6913785e10a7322932dfe72b5e4f02a7892bc850380f2638a"
+    sha256 big_sur:  "43e09456eaf522c380c9062668476dd1263d0e633458d901c13a1f85a6b069c7"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "bison" => :build
