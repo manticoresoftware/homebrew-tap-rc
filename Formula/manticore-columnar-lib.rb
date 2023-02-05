bd74b7fe08c9c8e4755982a5a07091966a3d246e
Don Hardman <59067742+donhardman@users.noreply.github.com>
2023-02-05 09:30:00 +0000

    manticore-columnar-lib: add 2.0.1-2023013023-f6df240 bottle.

--- Formula/manticore-columnar-lib.rb
+++ Formula/manticore-columnar-lib.rb
@@ -7,6 +7,12 @@ class ManticoreColumnarLib < Formula
   version_scheme 1
   head "https://github.com/manticoresoftware/columnar.git"
 
+  bottle do
+    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-columnar-lib-2.0.1-2023013023-f6df240"
+    sha256 cellar: :any_skip_relocation, monterey: "90cae17bae748fcc24e802cb00418c53734c02ea0fa997e0a7e47a3ef6cf377d"
+    sha256 cellar: :any_skip_relocation, big_sur:  "cb4f1df6a5666fb2921ca4bfb8fc3537ae7a7a0472faa1bee8fbe019c2c6e111"
+  end
+
   depends_on "boost" => :build
   depends_on "cmake" => :build
 
