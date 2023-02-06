6102392492a2e7a84a550b79a2d37a0813022446
Don Hardman <59067742+donhardman@users.noreply.github.com>
2023-02-06 18:41:22 +0700

    Update manticoresearch version

--- Formula/manticoresearch.rb
+++ Formula/manticoresearch.rb
@@ -1,8 +1,8 @@
 class Manticoresearch < Formula
   desc "Open source database for search"
   homepage "https://www.manticoresearch.com"
-  url "https://github.com/manticoresoftware/manticoresearch.git", branch: "manticore-6.0.0", revision: "7542cb76df91bcc3412082747ee3d651263dc36b"
-  version "6.0.0-2023020313-e12d6a9"
+  url "https://github.com/manticoresoftware/manticoresearch.git", branch: "manticore-6.0.0", revision: "8de9df2010d770331d2093d12e491a1868db5767"
+  version "6.0.0-2023020602-8de9df2"
   license "GPL-2.0"
   version_scheme 1
   head "https://github.com/manticoresoftware/manticoresearch.git"
