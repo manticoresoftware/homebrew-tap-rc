c73859b41648d65909d331f80b2f075ee1c7e331
Don Hardman <59067742+donhardman@users.noreply.github.com>
2023-02-06 15:45:38 +0000

    manticore-extra: add 0.6.2-2023020620-d95e43e bottle.

--- Formula/manticore-extra.rb
+++ Formula/manticore-extra.rb
@@ -8,6 +8,12 @@ class ManticoreExtra < Formula
   sha256 Digest::SHA256.file(File.expand_path(__FILE__)).hexdigest
   version_scheme 1
 
+  bottle do
+    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-extra-0.6.2-2023020620-d95e43e"
+    sha256 cellar: :any_skip_relocation, monterey: "31c3259dcee9bf43aa17d1b69e62f1d45fa65426507823341dac3f50275210ac"
+    sha256 cellar: :any_skip_relocation, big_sur:  "5d52633c3e757dd3a879cdec3a848bab7d073118826df0631c8d991bd7421446"
+  end
+
   depends_on "manticoresoftware/manticore/manticore-columnar-lib"
   depends_on "manticoresoftware/manticore/manticore-executor"
 
