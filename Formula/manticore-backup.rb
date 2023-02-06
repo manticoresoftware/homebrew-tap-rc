5ae2b1650881dbf0ba5e3c50ec040e48bcfe78a1
Don Hardman <don.hardman@muvon.io>
2023-02-06 10:51:31 +0000

    Update backup version to rebuild for release

--- Formula/manticore-backup.rb
+++ Formula/manticore-backup.rb
@@ -1,8 +1,8 @@
 class ManticoreBackup < Formula
   desc "Official tool for backups of Manticore Search"
   homepage "https://github.com/manticoresoftware/manticoresearch-backup"
-  url "https://github.com/manticoresoftware/manticoresearch-backup.git", revision: "4a3793286c03860ce1b0c46ebee6c6f58a1948de"
-  version "0.5.2-2023020221-4a37932"
+  url "https://github.com/manticoresoftware/manticoresearch-backup.git", branch: "0.5.2", revision: "4a3793286c03860ce1b0c46ebee6c6f58a1948de"
+  version "0.5.2-2023020222-4a37932"
   license "GPL-2.0"
   version_scheme 1
   head "https://github.com/manticoresoftware/manticoresearch-backup.git"
