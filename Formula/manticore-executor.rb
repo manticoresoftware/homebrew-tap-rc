d120573aaf9597599231c5d58e092e2ed4726586
Sergey Nikolaev <28719845+sanikolaev@users.noreply.github.com>
2023-02-07 05:45:21 +0000

    manticore-executor: update 0.6.2-2023020711-d95e43e bottle.

--- Formula/manticore-executor.rb
+++ Formula/manticore-executor.rb
@@ -8,9 +8,9 @@ class ManticoreExecutor < Formula
   head "https://github.com/manticoresoftware/executor.git"
 
   bottle do
-    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-executor-0.6.2-2023020620-d95e43e"
-    sha256 monterey: "bc994b626e0c1fa8746bdc15e7a0a0b41dcb8e810db8e07aff3d8d65f8cc04b4"
-    sha256 big_sur:  "b9c69e730cbfe2099935a1f1c0e15bc0cf4892db81bf2312fc4dc4e54c5a3d5b"
+    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-executor-0.6.2-2023020711-d95e43e"
+    sha256 monterey: "514924bf39e88751f4542599f1f1f17ab109728b6c0129b8aefc793a7f754a23"
+    sha256 big_sur:  "d8afb8258aa4569cbe228198ad88a9c35c134d6f2ec5cbfdf5b1ffa7e1e2e110"
   end
 
   depends_on "autoconf" => :build
