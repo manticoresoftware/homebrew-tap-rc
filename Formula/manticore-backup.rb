01acf784055b66faaf2d4a63778a55073a5d9ba6
Don Hardman <don.hardman@muvon.io>
2023-03-06 21:45:25 +0700

    Simplify building to fix issue with extracting PHAR on osx and update backup version

--- Formula/manticore-backup.rb
+++ Formula/manticore-backup.rb
@@ -1,17 +1,11 @@
 class ManticoreBackup < Formula
   desc "Official tool for backups of Manticore Search"
   homepage "https://github.com/manticoresoftware/manticoresearch-backup"
-  url "https://github.com/manticoresoftware/manticoresearch-backup.git", revision: "4a3793286c03860ce1b0c46ebee6c6f58a1948de"
-  version "0.5.3-2023020315-4a37932"
-  license "GPL-2.0"
+  url "https://github.com/manticoresoftware/manticoresearch-backup.git", revision: "d2c86cbd3b812ce36a04123412d65c783ca8e8a9"
+  version "0.5.3-2023030313-d2c86cb"
+  license "GPL-2.0-or-later"
   version_scheme 1
   head "https://github.com/manticoresoftware/manticoresearch-backup.git"
-
-  bottle do
-    root_url "https://github.com/manticoresoftware/homebrew-manticore/releases/download/manticore-backup-0.5.3-2023020315-4a37932"
-    sha256 cellar: :any_skip_relocation, monterey: "e3ce54fa79298143fbc8d90ad448aa87348bf074ba238ac995e1829db7a8020e"
-    sha256 cellar: :any_skip_relocation, big_sur:  "99174f3aa63587302cc64f07c0d0566ca6744fdb9f0760f4a17c88bdd9e536e7"
-  end
 
   depends_on "composer" => :build
   depends_on "php" => :build
@@ -19,7 +13,8 @@ class ManticoreBackup < Formula
 
   def install
     build_dir = `pwd`.strip + "/build"
-    system "./bin/build", "--name=\"Manticore Backup\"", "--package=manticore-backup", "--index=src/main.php"
+    system "git", "clone", "https://github.com/manticoresoftware/phar_builder.git"
+    system "./phar_builder/bin/build", "--name=\"Manticore Backup\"", "--package=manticore-backup", "--index=src/main.php"
     bin.install "#{build_dir}/manticore-backup" => "manticore-backup"
   end
 
