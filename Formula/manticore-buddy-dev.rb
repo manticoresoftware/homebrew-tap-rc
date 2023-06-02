6dc9ddf5f42a1e3c66d28cd921c6975455e52371
Don Hardman <Don Hardman>
2023-06-02 13:20:55 +0700

    Create bin path in share modules for buddy formula

--- Formula/manticore-buddy-dev.rb
+++ Formula/manticore-buddy-dev.rb
@@ -24,6 +24,7 @@ class ManticoreBuddyDev < Formula
   def install
     (share/"manticore").mkpath
     (share/"manticore/modules").mkpath
+    (share/"manticore/modules/bin").mkpath
     (lib/"manticore").mkpath
     share.install "share/modules/manticore-buddy" => "manticore/modules/manticore-buddy"
     share.install "bin/manticore-buddy" => "manticore/modules/manticore-buddy/bin/manticore-buddy"
