c8390046d1a4dc3a42bb4ec199a925acfe036af7
Don Hardman <don.hardman@muvon.io>
2023-07-25 16:58:56 +0700

    Add manticore-aot formula

---
+++ Formula/manticore-aot.rb
@@ -0,0 +1,15 @@
+class ManticoreAot < Formula
+  desc "Download and install Manticore morphology packs"
+  homepage "https://manticoresearch.com/"
+  url "https://manticoresearch.com"
+  version "0.9.9"
+
+  def install
+    (share/"manticore").mkpath
+
+    langs = %w[de en ru]
+    langs.each do |lang|
+      system "curl -sSL https://repo.manticoresearch.com/repository/morphology/#{lang}.pak.tgz | tar xvzf - -C #{share/"manticore"}"
+    end
+  end
+end
