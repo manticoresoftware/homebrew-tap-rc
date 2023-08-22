d2e4f0fbaf9fefb5bcef9a199f14f2e00eb1c135
Sergey Nikolaev <prostuda@academ.org>
2023-08-22 13:16:47 +0700

    RC formulas

---
+++ Formula/manticore-backup-rc.rb
@@ -0,0 +1,32 @@
+require_relative 'manticore_helper'
+require 'hardware'
+require "fileutils"
+
+class ManticoreBackupRc < Formula
+  desc "Manticore Search backup tool"
+  homepage "https://github.com/manticoresoftware/manticoresearch-backup"
+  license "GPL-2.0"
+
+  arch = Hardware::CPU.arch
+  base_url = 'https://repo.manticoresearch.com/repository/manticoresearch_macos/release_candidate/'
+  fetched_info = ManticoreHelper.fetch_version_and_url(
+    'manticore-backup',
+    base_url,
+    /(manticore-backup_)(\d+\.\d+\.\d+)(\-)(\d+\-)([\w]+)(\.tar\.gz)/
+  )
+
+  version fetched_info[:version]
+  url fetched_info[:file_url]
+  sha256 fetched_info[:sha256]
+
+  def install
+    (share/"manticore").mkpath
+    share.install "share/modules" => "manticore/modules"
+    bin.install "bin/manticore-backup" => "manticore-backup"
+  end
+
+  test do
+    File.file? "#{share}/manticore/modules/manticore-backup/src/main.php"
+    File.file? "#{bin}/manticore-backup"
+  end
+end
