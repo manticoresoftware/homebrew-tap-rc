9d6964f1103d4ddd9b01dcb408908c8dea5e30cf
Sergey Nikolaev <prostuda@academ.org>
2023-11-09 21:28:32 +0700

    Update manticoresearch.rb: fixed hash for x86_64
    Related issue manticoresoftware/manticoresearch#1575

--- Formula/manticore-columnar-lib.rb
+++ Formula/manticore-columnar-lib.rb
@@ -8,8 +8,13 @@ class ManticoreColumnarLib < Formula
   arch = Hardware::CPU.arch
   version "2.2.4-230822-5aec342"
   url "https://repo.manticoresearch.com/repository/manticoresearch_macos/release/manticore-columnar-lib-#{version}-osx11.6-#{arch}.tar.gz"
-  sha256 "c26c557b1359cd79862ead962b33f3910e7bca4d85344c99fa58ae34d1947dbb"
 
+  if arch.to_s == "x86_64" || arch.to_s == "amd64"
+    sha256 "2cbd0b7eb846208253f245ede729a42961f61d6a85adcbd78236c54f16ed0291"
+  else
+    sha256 "c26c557b1359cd79862ead962b33f3910e7bca4d85344c99fa58ae34d1947dbb"
+  end
+  
   def install
     (share/"manticore/modules").mkpath
     share.install "usr/local/share/manticore/modules/lib_manticore_columnar.so" => "manticore/modules/lib_manticore_columnar.so"
